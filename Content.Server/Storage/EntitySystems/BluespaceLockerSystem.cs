398e72451def3dbbf7f751f7cfc7230dc5e358fe
crazybrain23 <44417085+crazybrain23@users.noreply.github.com>
2024-12-27 12:34:30 +0000

    Arrivals blacklist for bluespace lockers and QSIs (#34072)
    * Ensure Arrivals Blacklist in Bluespace Locker rule
    
    * While I'm at it, stop the QSI too
    
    * fix thing I broke somehow
    
    * Every bluespace locker arrivals blacklisted
    
    * Add ArrivalsBlacklist to the prototypes too

--- Content.Server/Storage/EntitySystems/BluespaceLockerSystem.cs
+++ Content.Server/Storage/EntitySystems/BluespaceLockerSystem.cs
@@ -17,7 +17,6 @@ using Robust.Shared.Random;
 using Robust.Shared.Timing;
 using Robust.Shared.Prototypes;
 using Content.Server.Shuttles.Components;
-using Robust.Shared.Physics;
 
 namespace Content.Server.Storage.EntitySystems;
 
@@ -95,7 +94,7 @@ public sealed class BluespaceLockerSystem : EntitySystem
             if (component.BehaviorProperties.TransportEntities || component.BehaviorProperties.TransportSentient)
                 foreach (var entity in target.Value.storageComponent.Contents.ContainedEntities.ToArray())
                 {
-                    if (HasComp<MindContainerComponent>(entity))
+                    if (EntityManager.HasComponent<MindContainerComponent>(entity))
                     {
                         if (!component.BehaviorProperties.TransportSentient)
                             continue;
@@ -164,11 +163,11 @@ public sealed class BluespaceLockerSystem : EntitySystem
             return false;
 
         if (lockerComponent.PickLinksFromSameMap &&
-            _transformSystem.GetMapId(link.ToCoordinates()) != _transformSystem.GetMapId(locker.ToCoordinates()))
+            link.ToCoordinates().GetMapId(EntityManager) != locker.ToCoordinates().GetMapId(EntityManager))
             return false;
 
         if (lockerComponent.PickLinksFromStationGrids &&
-            !HasComp<StationMemberComponent>(_transformSystem.GetGrid(link.ToCoordinates())))
+            !HasComp<StationMemberComponent>(link.ToCoordinates().GetGridUid(EntityManager)))
             return false;
 
         if (lockerComponent.PickLinksFromResistLockers &&
@@ -312,7 +311,7 @@ public sealed class BluespaceLockerSystem : EntitySystem
         if (component.BehaviorProperties.TransportEntities || component.BehaviorProperties.TransportSentient)
             foreach (var entity in entityStorageComponent.Contents.ContainedEntities.ToArray())
             {
-                if (HasComp<MindContainerComponent>(entity))
+                if (EntityManager.HasComponent<MindContainerComponent>(entity))
                 {
                     if (!component.BehaviorProperties.TransportSentient)
                         continue;
@@ -393,7 +392,7 @@ public sealed class BluespaceLockerSystem : EntitySystem
         switch (component.BehaviorProperties.DestroyType)
         {
             case BluespaceLockerDestroyType.Explode:
-                _explosionSystem.QueueExplosion(_transformSystem.ToMapCoordinates(uid.ToCoordinates()),
+                _explosionSystem.QueueExplosion(uid.ToCoordinates().ToMap(EntityManager, _transformSystem),
                     ExplosionSystem.DefaultExplosionPrototypeId, 4, 1, 2, uid, maxTileBreak: 0);
                 goto case BluespaceLockerDestroyType.Delete;
             case BluespaceLockerDestroyType.Delete:
