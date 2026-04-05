1272e22b5251a8d58361fbd4565488b5d828ab95
Princess Cheeseballs <66055347+Princess-Cheeseballs@users.noreply.github.com>
2026-04-05 15:01:59 -0700

    Uplink implants connect to the same store as the PDA, and also fix not giving stores to PDAs (#43485)
    * WHISKEY ECHO WHISKEY
    
    * add fallback behavior
    
    * add comment
    
    * buy from the proper store ent
    
    ---------
    
    Co-authored-by: Princess Cheeseballs <66055347+Pronana@users.noreply.github.com>

--- Content.Server/Store/Systems/StoreSystem.cs
+++ Content.Server/Store/Systems/StoreSystem.cs
@@ -54,8 +54,9 @@ public sealed partial class StoreSystem : SharedStoreSystem
         SubscribeLocalEvent<StoreComponent, MapInitEvent>(OnMapInit);
         SubscribeLocalEvent<StoreComponent, ComponentStartup>(OnStartup);
         SubscribeLocalEvent<StoreComponent, ComponentShutdown>(OnShutdown);
-        SubscribeLocalEvent<StoreComponent, OpenUplinkImplantEvent>(OnImplantActivate);
         SubscribeLocalEvent<StoreComponent, IntrinsicStoreActionEvent>(OnIntrinsicStoreAction);
+
+        SubscribeLocalEvent<RemoteStoreComponent, OpenUplinkImplantEvent>(OnImplantActivate);
 
         InitializeUi();
         InitializeCommand();
@@ -131,9 +132,12 @@ public sealed partial class StoreSystem : SharedStoreSystem
         _popup.PopupEntity(msg, target, args.User);
     }
 
-    private void OnImplantActivate(EntityUid uid, StoreComponent component, OpenUplinkImplantEvent args)
+    private void OnImplantActivate(Entity<RemoteStoreComponent> entity, ref OpenUplinkImplantEvent args)
     {
-        ToggleUi(args.Performer, uid, component);
+        if (GetRemoteStore(entity.AsNullable()) is not { } store)
+            return;
+
+        ToggleUi(args.Performer, store, store.Comp, entity, entity.Comp);
     }
 
     /// <summary>
--- Content.Shared/PDA/Ringer/RingerUplinkComponent.cs
+++ Content.Shared/PDA/Ringer/RingerUplinkComponent.cs
@@ -19,10 +19,4 @@ public sealed partial class RingerUplinkComponent : Component
     /// </summary>
     [DataField]
     public bool Unlocked;
-
-    /// <summary>
-    /// The store which the ringer is targetting.
-    /// </summary>
-    [DataField]
-    public EntityUid? TargetStore;
 }
