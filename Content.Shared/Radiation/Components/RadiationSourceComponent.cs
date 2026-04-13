c6dff3c7f025a7534be44b84c8b9185a43e5e739
Princess Cheeseballs <66055347+Princess-Cheeseballs@users.noreply.github.com>
2026-04-13 15:42:02 -0700

    Fix two Radiation Bugs (#43504)

--- Content.Shared/Radiation/Components/RadiationSourceComponent.cs
+++ Content.Shared/Radiation/Components/RadiationSourceComponent.cs
@@ -4,6 +4,7 @@
 // SPDX-FileCopyrightText: 2026 Ilya Mikheev <me@ilyamikcoder.com>
 //
 // SPDX-License-Identifier: MIT
+using Content.Shared.Radiation.Systems;
 using Robust.Shared.Physics;
 
 namespace Content.Shared.Radiation.Components;
@@ -12,6 +13,7 @@ namespace Content.Shared.Radiation.Components;
 ///     Irradiate all objects in range.
 /// </summary>
 [RegisterComponent]
+[Access(typeof(SharedRadiationSystem))]
 public sealed partial class RadiationSourceComponent : Component
 {
     /// <summary>
