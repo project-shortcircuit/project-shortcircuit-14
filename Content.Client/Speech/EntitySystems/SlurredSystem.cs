6be6991a04f301a65daccbcb38e83549aff9f0bd
Pok <113675512+Pok27@users.noreply.github.com>
2026-04-25 04:59:28 +0300

    AllCaps accent and refactoring (#43666)
    * AllCapsStatusEffects
    
    * refactor
    
    * review
    
    * two minor changes
    
    ---------
    
    Co-authored-by: Princess Cheeseballs <66055347+Pronana@users.noreply.github.com>

--- Content.Client/Speech/EntitySystems/SlurredSystem.cs
+++ Content.Client/Speech/EntitySystems/SlurredSystem.cs
@@ -4,10 +4,16 @@
 //
 // SPDX-License-Identifier: MIT
 
+using Content.Shared.Speech.Components;
 using Content.Shared.Speech.EntitySystems;
+
 
 namespace Content.Client.Speech.EntitySystems;
 
 public sealed class SlurredSystem : SharedSlurredSystem
 {
+    protected override string AccentuateInternal(EntityUid uid, SlurredAccentComponent comp, string message)
+    {
+        return message;
+    }
 }
--- Content.Server/Speech/EntitySystems/SlurredSystem.cs
+++ Content.Server/Speech/EntitySystems/SlurredSystem.cs
@@ -9,9 +9,8 @@
 // SPDX-License-Identifier: MIT
 
 using System.Text;
-using Content.Server.Speech.Components;
 using Content.Shared.Drunk;
-using Content.Shared.Speech;
+using Content.Shared.Speech.Components;
 using Content.Shared.Speech.EntitySystems;
 using Content.Shared.StatusEffectNew;
 using Robust.Shared.Random;
@@ -35,13 +34,6 @@ public sealed class SlurredSystem : SharedSlurredSystem
     /// </summary>
     private const float SlurredThreshold = 80f;
 
-    public override void Initialize()
-    {
-        SubscribeLocalEvent<SlurredAccentComponent, AccentGetEvent>(OnAccent);
-
-        SubscribeLocalEvent<SlurredAccentComponent, StatusEffectRelayedEvent<AccentGetEvent>>(OnAccentRelayed);
-    }
-
     /// <summary>
     ///     Slur chance scales with the time remaining on any status effect with the SlurredAccentComponent.
     ///     Typically, this is equivalent to "drunkenness" on the DrunkStatusEffect
@@ -52,26 +44,15 @@ public sealed class SlurredSystem : SharedSlurredSystem
             return 0;
 
         // This is a magic number. Why this value? No clue it was made 3 years before I refactored this.
-        var magic = time.Item2 == null ? SlurredModifier : (float) (time.Item2 - _timing.CurTime).Value.TotalSeconds - SlurredThreshold;
+        var magic = time.Item2 == null ? SlurredModifier : (float)(time.Item2 - _timing.CurTime).Value.TotalSeconds - SlurredThreshold;
 
         return Math.Clamp(magic / SlurredModifier, 0f, 1f);
     }
 
-    private void OnAccent(Entity<SlurredAccentComponent> entity, ref AccentGetEvent args)
-    {
-        GetAccent(entity, ref args);
-    }
-
-    private void OnAccentRelayed(Entity<SlurredAccentComponent> entity, ref StatusEffectRelayedEvent<AccentGetEvent> args)
-    {
-        var ev = args.Args;
-        GetAccent(args.Args.Entity, ref ev);
-    }
-
-    private void GetAccent(EntityUid uid, ref AccentGetEvent args)
+    protected override string AccentuateInternal(EntityUid uid, SlurredAccentComponent comp, string message)
     {
         var scale = GetProbabilityScale(uid);
-        args.Message = Accentuate(args.Message, scale);
+        return Accentuate(message, scale);
     }
 
     private string Accentuate(string message, float scale)
@@ -110,7 +91,7 @@ public sealed class SlurredSystem : SharedSlurredSystem
                 }
             }
 
-            if (!_random.Prob(scale * 3/20))
+            if (!_random.Prob(scale * 3 / 20))
             {
                 sb.Append(character);
                 continue;
